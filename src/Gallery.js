000876460f43ccc7eb549235a6ac98f04e4daee9
Ella <ella.andersson@tuni.fi>
2022-01-30 18:31:31 +0200

    commit all files

--- src/Gallery.js
+++ src/Gallery.js
@@ -14,7 +14,7 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 
-
+//gallery for edited images
 const Gallery = ({setSelectedImg}) =>{
     const [anchorEl, setAnchorEl] = React.useState(null);
     const [backgroundCOpen,setBackgroundCOpen] = React.useState(false);
